0eee0a58dbc588a85adfabb85af4945649b53cfa
Daniele Valentino <d.valentino5@studenti.unisa.it>
2022-06-29 11:55:34 +0200

    bug fixed

--- speech_ws/src/speech_pkg/src/manager_node.py
+++ speech_ws/src/speech_pkg/src/manager_node.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-
+from multiprocessing import Manager
 import rospy
 from speech_pkg.srv import *
 
 def run(req):
-    res = classify(req.data)
-    cmd, probs = res.cmd, res.probs
-    res = speech(cmd, probs)
-    return ManagerResponse(res.flag)
+    res = transcribe(req.data)
+    out_string = res.s
+    print(out_string)
+    #res = speech(out_string)
+    #return ManagerResponse(res.flag)
+    return ManagerResponse(True)
 
 if __name__ == "__main__":
     rospy.init_node('manager')
-    rospy.wait_for_service('classifier_service')
-    rospy.wait_for_service('speech_service')
+    rospy.wait_for_service('transcriber_service')
+    #rospy.wait_for_service('speech_service')
     rospy.Service('manager_service', Manager, run)
-    classify = rospy.ServiceProxy('classifier_service', Classification)
-    speech = rospy.ServiceProxy('speech_service', Talker)
+    transcribe = rospy.ServiceProxy('transcriber_service', Transcription)
+    #speech = rospy.ServiceProxy('speech_service', Talker)
     rospy.spin()
