0104ab06692628e7783cb547b781527bf223bf44
Daniele Valentino <d.valentino5@studenti.unisa.it>
2022-07-19 23:13:00 +0200

    added flask

--- speech_ws/src/pepper_nodes/src/tablet_service.py
+++ speech_ws/src/pepper_nodes/src/tablet_service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from pepper_nodes.srv import ExecuteJS, ExecuteJSResponse
+from pepper_nodes.srv import ExecuteJS, ExecuteJSResponse, LoadUrl, LoadUrlResponse
 import qi
 import rospy
 IP = "10.0.1.230" # 10.0.1.230
@@ -10,20 +10,30 @@ PORT = 9559
 def callback(req):
     transcription = str(req.text)
     text = '''
+    <!DOCTYPE html>
     <html>
-        <body bgcolor="#800000">
-            <h1>{}</h1>
-        </body>
-    </html>
-    '''.format(transcription)
+    <head>
+    <style>
+    body {
+      background-color: #CD5C5C;
+    }
+    </style>
+    </head>
+    <body>
 
-    #file = open("/home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html","w")
-    #file.write(text)
-    #file.close()
-    #update_html(transcription)
-    print("html updated")
+    <h1>{trans}</h1>
+    <p><a href="https://www.w3schools.com">Visit W3Schools.com!</a></p>
+
+    </body>
+    </html>
+    '''.format(trans = transcription)
+
+    file = open("/home/speech_ws/webapp/templates/index.html","w")
+    file.write(text)
+    file.close()
+    print("file html updated")
     show(transcription)
-    return ExecuteJSResponse("ACK0")
+    return LoadUrlResponse("ACK0")
 
 def connect_robot():
     # Connect to the robot
@@ -39,28 +49,25 @@ def connect_robot():
     tablet = session.service("ALTabletService")
     tablet.resetTablet()
     #tablet.showWebview("http://198.18.0.1/apps/boot-config/preloading_dialog.html")
-    #tablet.showWebview()
     return tablet
 
 def show(out_str):
     try:
-        #tablet.executeJS("/home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html")
         #tablet.loadUrl("https://www.unisa.it")
         tablet.loadUrl("http://127.0.0.1:5001/")
         tablet.showWebview()
-        #tablet.executeJS(script)
+        print("HTML loaded")
     except Exception:
         session = qi.Session()
         session.connect('tcp://%s:9559' % IP )
         tablet = session.service("ALTabletService")
         tablet.loadUrl("http://127.0.0.1:5001/")
         tablet.showWebview()
-        #tablet.executeJS(script)
     # time.sleep(0.5)
 
 if __name__ == "__main__":
     tablet = connect_robot()
     rospy.init_node('tablet_service')
-    rospy.Service('tablet_server', ExecuteJS, callback)
+    rospy.Service('tablet_server', LoadUrl, callback)
     print("Ready to update the tablet screen.")
     rospy.spin()
--- speech_ws/src/ros_audio_pkg/src/pepper_interface_node.py
+++ speech_ws/src/ros_audio_pkg/src/pepper_interface_node.py
@@ -2,7 +2,7 @@
 import rospy
 from std_msgs.msg import Int16MultiArray, String
 import numpy as np
-from pepper_nodes.srv import ExecuteJS, Text2Speech
+from pepper_nodes.srv import ExecuteJS, Text2Speech, LoadUrl
 from flask import Flask
 
 # Init node
@@ -10,7 +10,7 @@ rospy.init_node('pepper_interface_node', anonymous=True)
 
 #tts = rospy.ServiceProxy('gtts_server', TextToSpeechGTTS) #gTTS with Audacious
 tts = rospy.ServiceProxy('tts_server', Text2Speech) #TTS with Pepper
-tablet = rospy.ServiceProxy('tablet_server', ExecuteJS) # Pepper tablet
+tablet = rospy.ServiceProxy('tablet_server', LoadUrl) # Pepper tablet
 pub = rospy.Publisher('tts_ack', String, queue_size=1)
 
 def callback(text):
