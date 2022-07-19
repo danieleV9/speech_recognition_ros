2cc3933e4d8d7abf5d8faf19c7a2c2d4b9431354
Daniele Valentino <d.valentino5@studenti.unisa.it>
2022-07-19 16:24:45 +0200

    bug

--- speech_ws/src/pepper_nodes/src/tablet_service.py
+++ speech_ws/src/pepper_nodes/src/tablet_service.py
@@ -42,14 +42,15 @@ def connect_robot():
 def show(out_str):
     try:
         #tablet.executeJS("/home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html")
-        tablet.loadUrl("https://www.unisa.it")
+        #tablet.loadUrl("https://www.unisa.it")
+        tablet.loadUrl("file:///home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html")
         tablet.showWebview()
     except Exception:
         session = qi.Session()
         session.connect('tcp://%s:9559' % IP )
         tablet = session.service("ALTabletService")
         #tablet.executeJS("/home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html")
-        tablet.loadUrl("https://www.unisa.it")
+        tablet.loadUrl("file:///home/speech_ws/src/pepper_nodes/src/j-tablet-browser/index.html")
         tablet.showWebview()
     # time.sleep(0.5)
 
